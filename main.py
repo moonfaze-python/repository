960b2ba5800621cb7161ad5596b791038ceba208
moonfaze-python <ilyuchasan@gmail.com>
2026-07-10 20:36:11 +0300

    коммит самостоятельный

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 #pip install fastapi uvicorn
-#uvicorn main:app --port 8080 --reload
+#uvicorn main:app --port 8000 --reload
 #ctrl+shift+I открыть DevTools
+#git add .
+#git commit -m "Первый коммит"
+#git push -u origin master
 from fastapi import FastAPI
 from pydantic import BaseModel
 from uuid import uuid4
 from fastapi.middleware.cors import CORSMiddleware
+
+book = ''
 
 app = FastAPI()
 
@@ -33,3 +38,17 @@ def create_task(payload: TaskCreateSchema) -> TaskSchema:
     new_task = TaskSchema(id=str(uuid4()),title=payload.title,completed=False)
     tasks.append(new_task)
     return new_task
+
+class TaskCreateBook(BaseModel):
+    book : str
+
+@app.post('/book')
+def create_book(payload: TaskCreateBook):
+    global book
+    book = payload.book
+    return book
+
+@app.get('/book')
+def read_book():
+    global book
+    return f'Любимая книга {book}'
